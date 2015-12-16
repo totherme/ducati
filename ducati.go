78666031931173c5dc11fa3d49e146b4fc0be3bd
Gabe Rosenhouse <grosenhouse@pivotal.io>
2015-12-16 09:41:08 -0800

    Ducati does not need to match Networker interface

--- ducati.go
+++ ducati.go
@@ -3,7 +3,6 @@ package ducati
 import (
 	"errors"
 
-	"github.com/cloudfoundry-incubator/guardian/gardener"
 	"github.com/pivotal-golang/lager"
 )
 
@@ -14,5 +13,3 @@ func (d *Ducati) Capacity() uint64 { return 0 }
 func (d *Ducati) Network(log lager.Logger, handle, spec string) (string, error) {
 	return "", errors.New("ducati not implemented")
 }
-
-var _ gardener.Networker = &Ducati{}
