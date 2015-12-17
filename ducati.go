f0cc6690bffaf67c862e752fde69e625a905024e
Georgi Sabev <georgethebeatle@gmail.com>
2015-12-17 14:56:11 +0000

    Add stubs to bring ducati up to date with gardener.Networker
    Signed-off-by: Gareth Smith <gsmith@pivotal.io>

--- ducati.go
+++ ducati.go
@@ -13,3 +13,11 @@ func (d *Ducati) Capacity() uint64 { return 0 }
 func (d *Ducati) Network(log lager.Logger, handle, spec string) (string, error) {
 	return "", errors.New("ducati not implemented")
 }
+
+func (d *Ducati) Destroy(log lager.Logger, handle string) error {
+	return errors.New("ducati not implemented")
+}
+
+func (d *Ducati) NetIn(handle string, hostPort, containerPort uint32) (uint32, uint32, error) {
+	return 0,0,errors.New("ducati not implemented")
+}
